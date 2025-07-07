09fe6bd173e935340062fccbad2ebcc5b9e58ea1
João Guilherme Paiva <38386145+jguilhermepaiva@users.noreply.github.com>
2025-07-06 21:47:39 -0300

    improve code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,18 @@
-// /server/src/server.ts
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-import * as express from 'express';
+import express = require('express');
 import * as cors from 'cors';
 import userRoutes from './routes/userRoutes';
 import groupRoutes from './routes/groupRoutes';
 
-// Cria a aplicação Express
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
-// Monta as rotas de usuário no prefixo /api/users
 app.use('/api/users', userRoutes);
-
-// 2. Monta as novas rotas de grupo no prefixo /api/groups
 app.use('/api/groups', groupRoutes);
 
 app.listen(PORT, () => {
